fc977397c0bf97ee89885330ce25ca8643b58a0b
DevBensmaine <bensmaine.hassan@gmail.com>
2020-07-13 22:32:55 +0100

    create all component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OrdersService } from "./shared/orders.service";
+import { ReactiveFormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [OrdersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { OrdersService } from "../shared/orders.service";
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css'],
+})
+export class OrdersComponent implements OnInit {
+  constructor() {}
+  form = new FormGroup({
+    customerName: new FormControl(''),
+    orderNumber: new FormControl(''),
+    coffeeOrder: new FormControl(''),
+    completed: new FormControl(false),
+  });
+  ngOnInit(): void {}
+}
